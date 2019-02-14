65e396bb1f7d9804ddcef27d07d2e9dfed4dfe0d
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-02-14 10:43:26 +0100

    Add leak canary build type.

--- app/src/main/java/pl/gmat/dynamicgallery/DynamicGalleryApp.kt
+++ app/src/main/java/pl/gmat/dynamicgallery/DynamicGalleryApp.kt
@@ -1,6 +1,7 @@
 package pl.gmat.dynamicgallery
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import com.squareup.picasso.LruCache
 import com.squareup.picasso.Picasso
 
@@ -11,6 +12,10 @@ class DynamicGalleryApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.ENABLE_LEAK_CANARY) {
+            if (LeakCanary.isInAnalyzerProcess(this)) return
+            LeakCanary.install(this)
+        }
         picassoCache = LruCache(this)
         picasso = Picasso.Builder(this).memoryCache(picassoCache).build()
     }
